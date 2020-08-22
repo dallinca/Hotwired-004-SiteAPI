a7f33f7f745e0be6b3886b5b967f2531dc72cf4b
Dallin Andersen <andersen.dallin@gmail.com>
2020-08-21 18:19:40 -0600

    Added option and functionality to run as http or https

--- server.js
+++ server.js
@@ -4,6 +4,34 @@ var port = process.env.PORT || configIpPort.port;
 
 var ipv4 = configIpPort.ipv4;
 
-var server = app.listen(port, ipv4, function() {
+if (configIpPort.https == false) {
+
+	var http = require('http');
+
+	var httpServer = http.createServer(app);
+
+	httpServer.listen(3000, ipv4, function() {
+	  console.log('Express server listening on port ' + port);
+	});
+
+} else {
+	var fs = require('fs');
+	var https = require('https');
+
+	var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+	var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
+	var credentials = {key: privateKey, cert: certificate};
+
+	var httpsServer = https.createServer(credentials, app);
+
+	httpsServer.listen(3000, ipv4, function() {
+	  console.log('Express server listening on port ' + port);
+	});
+
+}
+
+/*var server = app.listen(port, ipv4, function() {
   console.log('Express server listening on port ' + port);
-});
+});*/
+
+
