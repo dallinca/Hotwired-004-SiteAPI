3067347103244a2938d1a035d9a353db2d49e950
Dallin Andersen <andersen.dallin@gmail.com>
2022-06-23 15:59:43 -0600

    removed old code/logs

--- utils/VerifyToken.js
+++ utils/VerifyToken.js
@@ -1,8 +1,6 @@
 // Set Root Folder path
 var pathToRootFolder = '../';
 
-//var adminjwt = require('jsonwebtoken');
-//var sitejwt = require('jsonwebtoken');
 var jwt = require('jsonwebtoken');
 var config = require(pathToRootFolder + 'config/config');
 const logger = require(pathToRootFolder + 'utils/logger');
@@ -19,21 +17,6 @@ function getPortal(req) {
 }
 
 function verifyToken(req, res, next) {
-  /*var portal = getPortal(req);
-  var jwt = {};
-  console.log(jwt);
-  if (portal == 'admin') {
-    jwt = adminjwt;
-    console.log('admin');
-  } else if (portal == 'site') {
-    jwt = sitejwt;
-    console.log('site');
-  } else {
-    return res.status(500).send({ auth: false, token: null, message: 'Unrecognized portal.' });
-  }
-  console.log(jwt);
-  console.log(jwt.toString());*/
-
   var token = req.headers['x-access-token'];
   if (!token) return res.status(403).send({ auth: false, token: null, message: 'No token provided.' });
     
@@ -41,7 +24,6 @@ function verifyToken(req, res, next) {
     if (err) return res.status(500).send({ auth: false, token: null, message: 'Failed to authenticate token.' });
     // if everything good, save to request for use in other routes
     res.locals.userId = decoded.id;
-    console.log(decoded.id);
     next();
   });
 }
