8edf1f028ec92a4f784f2027e7147a866561cef0
Dallin Andersen <andersen.dallin@gmail.com>
2022-06-28 18:38:16 -0600

    Story 24 - Added totalCount of users to the response for use in frontend pagination controls

--- api/v1/admin/user/UserController.js
+++ api/v1/admin/user/UserController.js
@@ -3,7 +3,7 @@ const {
   config,
   logger,
   verifyToken, cacheTokenOwnerInfo, verifyPermission,
-  errorCode, // nextErrorCode = '00009'; // Only used for keeping loose track of next ID assignment
+  errorCode, // nextErrorCode = '00010'; // Only used for keeping loose track of next ID assignment
   translations,
   router,
 } = require(global.appRoot + '/utils/standardUtils.js')(__filename);
@@ -49,16 +49,23 @@ router.get('/me', [verifyToken, cacheTokenOwnerInfo], function(req, res, next) {
 });
 
 router.get('/all', [verifyToken, cacheTokenOwnerInfo, verifyPermission(P.P_Admin_User_ViewAll), verifyPaginationParameters], function(req, res, next) {
-  User.find({}, { password: 0, _id: 0, __v: 0 }, { skip:parseInt(req.query.skip), limit:parseInt(req.query.limit), sort:'name' }, function(err, users) {
-    if (!users) {
-      return res.status(500).send({ auth: false, token: null, code: errorCode('00005'), message: translations(ERROR_UsersDoNotExit, res.locals.language) });
-    } else if (err) {
-      logger.error(`500 - ${errorCode('00006')} - ${err}`);
-      return res.status(500).send({ auth: false, token: null, code: errorCode('00006'), message: translations(ERROR_Server_Generic, res.locals.language) });
-    } else {
-      return res.status(200).send({ auth: true, token: null, message: translations(SUCCESS_User_DataProvided, res.locals.language), 'data': { 'users': users } });
+  User.countDocuments({}, function(err, count) {
+    if (err) {
+      logger.error(`500 - ${errorCode('00009')} - ${err}`);
+      return res.status(500).send({ auth: false, token: null, code: errorCode('00009'), message: translations(ERROR_Server_Generic, res.locals.language) });
     }
-  })
+    console.log("count: " + count)
+    User.find({}, { password: 0, _id: 0, __v: 0 }, { skip:parseInt(req.query.skip), limit:parseInt(req.query.limit), sort:'name' }, function(err, users) {
+      if (!users) {
+        return res.status(500).send({ auth: false, token: null, code: errorCode('00005'), message: translations(ERROR_UsersDoNotExit, res.locals.language) });
+      } else if (err) {
+        logger.error(`500 - ${errorCode('00006')} - ${err}`);
+        return res.status(500).send({ auth: false, token: null, code: errorCode('00006'), message: translations(ERROR_Server_Generic, res.locals.language) });
+      } else {
+        return res.status(200).send({ auth: true, token: null, message: translations(SUCCESS_User_DataProvided, res.locals.language), 'data': { 'count': count, 'users': users } });
+      }
+    })
+  });
 })
 
 router.get('/_:userId', [verifyToken, cacheTokenOwnerInfo, verifyPermission(P.P_Admin_User_ViewOne)], function(req, res, next) {
