f818867706e3886454cb46d4b8be42bf44cf37ee
Dallin Andersen <andersen.dallin@gmail.com>
2022-07-02 11:50:11 -0600

    Improve English error message

--- api/v1/admin/visitor/VisitorController.js.lang/en.js
+++ api/v1/admin/visitor/VisitorController.js.lang/en.js
@@ -2,7 +2,7 @@ const NAMES = require('./names.js');
 
 module.exports = {
     [NAMES.ERROR_Server_Generic]: 'Error on the server.',
-    [NAMES.ERROR_Email_AlreadyUsed]: 'Email already in use. Please use another email.',
+    [NAMES.ERROR_Email_AlreadyUsed]: 'Email already in use.',
     [NAMES.ERROR_Email_NotAVisitor]: 'Email provided is not currently registered as a visitor',
     [NAMES.ERROR_Email_NotProvided]: 'Request Body must contain an email field.',
     [NAMES.ERROR_Visitor_Approval_NotProvided]: 'Request Body must contain the boolean approveUser field',
